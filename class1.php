f31c12d22c6d045e5cfabe5dcb95648e52ce5a1d
Heet Thummar <thummarhit6271@gmail.com>
2023-07-14 09:17:54 +0530

    qqq

---
+++ class1.php
@@ -0,0 +1,14 @@
+<?php
+    class class1{
+        private $font_color;
+        private $font_text;
+        function __construct($f,$s){
+            $this->font_color=$f;
+            $this->font_text=$s;
+            $this->print_msg();
+        }
+        function print_msg(){
+ echo "<p style=color:".$this->font_color.";>".$this->font_text."</p>";
+        }
+    }
+?>
